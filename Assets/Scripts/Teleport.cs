2f0eb27176ac9a432f3f7a0bc487d1d1eb096f0b
Aysenur Turan <nurayse.turan@gmail.com>
2023-07-27 19:14:25 +0300

    developed mechanics

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -7,9 +7,9 @@ public class Teleport : MonoBehaviour
     public Transform teleportTarget; // Işınlanılacak hedef nokta
     public bool requiresKey = true; // Işınlanmak için anahtar gerekiyor mu?
 
-    private void OnCollisionEnter2D(Collision2D point)
+    private void OnTriggerEnter2D(Collider2D point)
     {
-        if (point.gameObject.CompareTag("Player"))
+        if (point.gameObject.CompareTag("Recordable"))
         {
             Movement playerMovement = point.gameObject.GetComponent<Movement>();
             if (playerMovement != null && (!requiresKey || playerMovement.HasKey()))
